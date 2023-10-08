07743574cfe7f69edb3ffb48990cfc6d206163f3
Glitch <glitch.txs@gmail.com>
2023-10-08 18:26:29 -0300

    config

--- theme.config.jsx
+++ theme.config.jsx
@@ -1,20 +1,27 @@
 export default {
   logo: <span>Ronin Builders</span>,
 
-  project: ()=>{
-    const { asPath } = useRouter()
-    console.log("theme config path: ", asPath) // conditione the GitHub link depending on the library
-    return {
-      link: 'https://github.com/ronin-builders'
-    }
+  project: {
+    link: 'https://github.com/ronin-builders'
+  },
+  docsRepositoryBase:"https://github.com/ronin-builders/website",
+  footer: {
+    text: `MIT ${new Date().getFullYear()} © Ronin Builders.`
+  },
+  feedback: {
+    content: "Open an issue in the docs repository"
   },
   head: (
     <>
       <title>Ronin Builders | Community</title>
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <link rel="shortcut icon" href="/Ronin_Mark_Neon.svg" />
+      <meta property="og:image" content="/homepage-hero.png"/>
       <meta property="og:title" content="Ronin Builders" />
+      {/* <meta property="og:url" content="http://" /> */}
       <meta property="og:description" content="Typescript tools for Ronin Network. Community driven organization." />
+
+      <meta name="twitter:card" content="summary_large_image" />
     </>
   )
 }
