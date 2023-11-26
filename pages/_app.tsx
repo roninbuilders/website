04273413ac642de7e3a08721950f01c95012d88a
Glitch <glitch.txs@gmail.com>
2023-11-26 03:36:13 -0300

    downgrade dep

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,16 @@
+import { AppProps } from 'next/app';
 import '../styles/global.css'
 import NextNProgress from 'nextjs-progressbar';
 
+const nextProps = {
+  color: '#2662d9',
+  options: { showSpinner: false }
+}
 // This default export is required in a new `pages/_app.js` file.
-export default function MyApp({ Component, pageProps }) {
+export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
-    <NextNProgress color='#2662d9' options={{ showSpinner: false }} />
+    <NextNProgress {...nextProps} />
     <Component {...pageProps} />
     </>
   )
