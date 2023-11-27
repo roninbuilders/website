7cead70a9eb88019ed9c379be05960e622ffa0bb
Glitch <glitch.txs@gmail.com>
2023-11-27 04:13:13 -0300

    remove w3vm docs

--- components/Button.tsx
+++ components/Button.tsx
@@ -10,7 +10,7 @@ export default function Button({label, target}: Props) {
   return (
     <Link className={s.button} href={target} >
       {label}
-      <svg color="currentColor" viewBox="0 0 24 24" width="16" height="16" fill="currentcolor"><path fill-rule="evenodd" clip-rule="evenodd" d="m15.396 17.018-1.414-1.415 3.104-3.103H4.5a1 1 0 1 1 0-2h12.586l-3.104-3.104 1.414-1.414 5.518 5.518-5.518 5.518Z" fill=""></path></svg>
+      <svg color="currentColor" viewBox="0 0 24 24" width="16" height="16" fill="currentcolor"><path fillRule="evenodd" clipRule="evenodd" d="m15.396 17.018-1.414-1.415 3.104-3.103H4.5a1 1 0 1 1 0-2h12.586l-3.104-3.104 1.414-1.414 5.518 5.518-5.518 5.518Z" fill=""></path></svg>
     </Link>
   )
 }
