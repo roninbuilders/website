f8f391246db520f9c4cd9a67c2fddb4577c73592
Glitch <glitch.txs@gmail.com>
2023-12-28 00:03:59 -0300

    update modal dep

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -3,6 +3,7 @@ import { saigon, createRoninModal } from "@roninbuilders/modal"
 createRoninModal({
 	chain: saigon,
 	projectId: "cdbd18f9f96172be74c3e351ce99b908",
+	darkMode: true
 })
 
 export default function Modal() {
