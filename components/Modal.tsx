4775b237ac92737b41543697b98f514c1e52dfa2
glitch-txs <glitch.txs@gmail.com>
2025-02-01 21:18:05 -0300

    lock

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -3,7 +3,11 @@ import { saigon, createRoninModal } from "@roninbuilders/modal"
 createRoninModal({
 	chain: saigon,
 	projectId: "cdbd18f9f96172be74c3e351ce99b908",
-	darkMode: true
+	darkMode: true,
+	waypoint:{
+		clientId: "0e188f93-b419-4b0f-8df4-0f976da91ee6",
+		chainId: 2020,
+	}
 })
 
 export default function Modal() {
