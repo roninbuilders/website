d40d4480ee3a7b6cfbdeb4d9aeea405a070233ed
Glitch <glitch.txs@gmail.com>
2025-02-18 10:43:30 -0300

    fix config

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -6,8 +6,7 @@ createRoninModal({
 	projectId: "cdbd18f9f96172be74c3e351ce99b908",
 	darkMode: true,
 	waypoint:{
-		clientId: "0e188f93-b419-4b0f-8df4-0f976da91ee6",
-		chainId: 2020,
+		clientId: "0e188f93-b419-4b0f-8df4-0f976da91ee6"
 	}
 })
 
